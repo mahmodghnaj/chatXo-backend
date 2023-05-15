9e3aace2721b3706d333b3ae403d8a2763cd2b46
Mahmod <mahmodghnaj1@gmail.com>
2023-05-15 18:20:04 +0300

    fix names

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -4,7 +4,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import * as bcrypt from 'bcrypt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { User } from './entities/user.entity';
+import { User } from './schemas/user.schema';
 
 @Injectable()
 export class UsersService {
